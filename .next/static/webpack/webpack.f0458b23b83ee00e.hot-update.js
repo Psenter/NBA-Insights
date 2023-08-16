c7fa9b7ffdd3f2af1b6148721d780300c2c6acd1
Parker S <111610335+Psenter@users.noreply.github.com>
2023-08-16 18:17:58 -0400

    Got navbar to be able to swap from team view back to home

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "00396a802583fc14"; }
+/******/ 	__webpack_require__.h = function() { return "02f1b4e3b60ae03c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
+import CustomNavbar from "../components/Navbar";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Page() {
   return <div>
   <Header/>
-  <Navbar/>
+  <CustomNavbar/>
   </div>
 }
