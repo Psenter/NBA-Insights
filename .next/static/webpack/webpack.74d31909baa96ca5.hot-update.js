c27f25187df3e11540a0651a890372bc438507f4
Parker S <111610335+Psenter@users.noreply.github.com>
2023-08-26 19:48:04 -0400

    Can now view favorites page and delete favorites from the page.

--- .next/static/webpack/webpack.74d31909baa96ca5.hot-update.js
+++ .next/static/webpack/webpack.74d31909baa96ca5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fdb2607609e43cfe"; }
+/******/ 	__webpack_require__.h = function() { return "7a3860dbade97e71"; }
 /******/ }();
 /******/ 
 /******/ }
--- src/components/FavoritesComponent.js
+++ src/components/FavoritesComponent.js
@@ -1,53 +1,98 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Link from "next/link";
-import CustomNavbar from "./Navbar";
 
-function FavoritesDisplay() {
+function FavoritesComponent() {
     const [teamFavorites, setTeamFavorites] = useState([]);
     const [playerFavorites, setPlayerFavorites] = useState([]);
+    const [teamNameMap, setTeamNameMap] = useState({});
+    const [playerNameMap, setPlayerNameMap] = useState({});
 
     useEffect(() => {
         // Fetch favorite teams
         axios.get("http://127.0.0.1:8000/FavoriteTeams/?format=json")
             .then((response) => {
-                console.log("Team Favorites API Response:", response.data); // Log the response
                 setTeamFavorites(response.data);
+                const teamMap = {};
+                const teamPromises = response.data.map((favorite) => (
+                    axios.get(`http://127.0.0.1:8000/Teams/${favorite.team_id}/?format=json`)
+                        .then((teamResponse) => {
+                            teamMap[favorite.team_id] = teamResponse.data.team_name;
+                        })
+                        .catch((error) => {
+                            console.error("Error fetching team details:", error);
+                        })
+                ));
+
+                Promise.all(teamPromises).then(() => {
+                    setTeamNameMap(teamMap);
+                });
             })
             .catch((error) => {
-                console.error("Error fetching team favorites:", error);
+                console.error("Error fetching favorite teams:", error);
             });
-    
+
         // Fetch favorite players
         axios.get("http://127.0.0.1:8000/FavoritePlayers/?format=json")
             .then((response) => {
-                console.log("Player Favorites API Response:", response.data); // Log the response
                 setPlayerFavorites(response.data);
+                const playerMap = {};
+                const playerPromises = response.data.map((favorite) => (
+                    axios.get(`http://127.0.0.1:8000/Players/${favorite.player_id}/?format=json`)
+                        .then((playerResponse) => {
+                            playerMap[favorite.player_id] = `${playerResponse.data.first_name} ${playerResponse.data.last_name}`;
+                        })
+                        .catch((error) => {
+                            console.error("Error fetching player details:", error);
+                        })
+                ));
+
+                Promise.all(playerPromises).then(() => {
+                    setPlayerNameMap(playerMap);
+                });
             })
             .catch((error) => {
-                console.error("Error fetching player favorites:", error);
+                console.error("Error fetching favorite players:", error);
             });
     }, []);
-    
+
+    const handleDeleteTeam = (teamId) => {
+        axios.delete(`http://127.0.0.1:8000/FavoriteTeams/${teamId}/`)
+            .then(() => {
+                setTeamFavorites((prevFavorites) => prevFavorites.filter(team => team.id !== teamId));
+            })
+            .catch((error) => {
+                console.error("Error deleting favorite team:", error);
+            });
+    };
+
+    const handleDeletePlayer = (playerId) => {
+        axios.delete(`http://127.0.0.1:8000/FavoritePlayers/${playerId}/`)
+            .then(() => {
+                setPlayerFavorites((prevFavorites) => prevFavorites.filter(player => player.id !== playerId));
+            })
+            .catch((error) => {
+                console.error("Error deleting favorite player:", error);
+            });
+    };
 
     return (
         <div>
-            <h2>Favorite Teams</h2>
+            <h2>Favorite Teams:</h2>
             <ul>
-                {teamFavorites.map((favorite) => (
-                    <li key={favorite.id}>
-                        {/* Display team details */}
-                        {favorite.team_name}
+                {teamFavorites.map((team) => (
+                    <li key={team.id}>
+                        {teamNameMap[team.team_id]}
+                        <button onClick={() => handleDeleteTeam(team.id)}>Delete</button>
                     </li>
                 ))}
             </ul>
 
-            <h2>Favorite Players</h2>
+            <h2>Favorite Players:</h2>
             <ul>
-                {playerFavorites.map((favorite) => (
-                    <li key={favorite.id}>
-                        {/* Display player details */}
-                        {favorite.player_name}
+                {playerFavorites.map((player) => (
+                    <li key={player.id}>
+                        {playerNameMap[player.player_id]}
+                        <button onClick={() => handleDeletePlayer(player.id)}>Delete</button>
                     </li>
                 ))}
             </ul>
@@ -55,4 +100,4 @@ function FavoritesDisplay() {
     );
 }
 
-export default FavoritesDisplay;
+export default FavoritesComponent;
